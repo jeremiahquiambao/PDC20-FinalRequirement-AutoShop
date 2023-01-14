a5ea275f76a74522b0be70d96e4566febc2adb8e
jeremiahquiambao <quiambao.jeremiah@auf.edu.ph>
2023-01-15 02:47:26 +0800

    angular.js added

---
+++ js/angular.js
@@ -0,0 +1,17 @@
+function myFunction1() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
+
+  function myFunction2() {
+    var x = document.getElementById("confirm_password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
